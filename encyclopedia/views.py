690de581a65197580d9b03cba5bfa786858f6bda
Kodo Sawaki <ipalnermoani@gmail.com>
2021-08-07 20:53:09 +0300

    Add notfound entry templates and views, change index

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
-
+from markdown2 import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def wiki(request, title):
+    if title in util.list_entries():
+        content = markdown(util.get_entry(title))
+        return render(request, 'encyclopedia/entry.html', {
+            'title': title,
+            'content': content
+        })
+    else:
+        return render(request, 'encyclopedia/notfound.html')
