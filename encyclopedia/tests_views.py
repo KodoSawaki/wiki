1b92e41008393bae66394093a725296ceb8514c6
Kodo Sawaki <ipalnermoani@gmail.com>
2021-08-09 22:07:51 +0300

    Add tests for index view

--- encyclopedia/tests_views.py
+++ encyclopedia/tests_views.py
@@ -1,1 +1,16 @@
+import re
+from wiki.settings import SECRET_KEY
 from django.test import TestCase
+from django.urls import reverse
+
+from encyclopedia.util import list_entries
+
+class IndexViewTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('encyclopedia:index'))
+    def test_index_default_response(self):
+       '''Default index response should be 200''' 
+       self.assertEqual(self.response.status_code, 200)
+    def test_index_entries_list(self):   
+        '''Entries list displayed on the index page'''
+        self.assertListEqual(self.response.context['entries'],list_entries())
