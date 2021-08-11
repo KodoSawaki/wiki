13fcb47eb6359ba3d5fb42bcca3d607e5138e4a9
Kodo Sawaki <ipalnermoani@gmail.com>
2021-08-11 20:48:20 +0300

    Add tests for the wiki view

--- encyclopedia/tests_views.py
+++ encyclopedia/tests_views.py
@@ -1,5 +1,3 @@
-import re
-from wiki.settings import SECRET_KEY
 from django.test import TestCase
 from django.urls import reverse
 
@@ -14,3 +12,16 @@ class IndexViewTests(TestCase):
     def test_index_entries_list(self):   
         '''Entries list displayed on the index page'''
         self.assertListEqual(self.response.context['entries'],list_entries())
+
+class WikiViewTests(TestCase):
+    def test_wiki_entry_exists(self):
+        '''Loads entry page from md file'''
+        response = self.client.get(reverse('encyclopedia:wiki', args = ['Git']))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Git')
+    
+    def test_wiki_no_entry(self):
+        '''In case entry doesn't exist wiki loads notfound page'''
+        response = self.client.get('/wiki/iow3jerf2/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Encyclopedia doesn't have an article with this exact name.")
